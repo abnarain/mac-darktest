9e45186d3e6b1c75766b7db3ddbad7ade9091dc7
abhinav narain <nabhinav3@gatech.edu>
2012-03-12 21:26:00 +0000

    create has the kernel stats

--- src/create-interface.c
+++ src/create-interface.c
@@ -6,7 +6,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <linux/if_ether.h>
-#include <netpacket/packet.h>
 #include <net/ethernet.h>
 #include <netinet/ether.h>
 #include <arpa/inet.h>
@@ -17,9 +16,15 @@
 #include <linux/wireless.h>
 #include <errno.h>
 #include <math.h>
+#include <linux/if_packet.h>
 
 typedef unsigned char      uchar; 
 
+
+int64_t timeval_to_int64(const struct timeval* tv)
+{
+  return (int64_t)(((u_int64_t)(*tv).tv_sec)* 1000000ULL + ((u_int64_t)(*tv).tv_usec));
+}
 
 int config_radio_interface(const char device[])
 {
@@ -27,14 +32,13 @@ int config_radio_interface(const char device[])
   struct iwreq    wrq;
   memset(&wrq, 0, sizeof(wrq));
   strncpy(wrq.ifr_name, device, IFNAMSIZ);
-  
   wrq.u.mode = IW_MODE_MONITOR;
   if (0 > ioctl(sd, SIOCSIWMODE, &wrq)) {
     printf("ioctl(SIOCSIWMODE) : %s\n", strerror(errno));
     syslog(LOG_ERR, "ioctl(SIOCSIWMODE): %s\n", strerror(errno));
     return 1;
   }
-	return 0;
+  return 0;
 }
 
 int up_radio_interface(const char device[])
@@ -56,8 +60,7 @@ int up_radio_interface(const char device[])
     printf("ioctl(SIOCSIFFLAGS) : %s\n", strerror(errno));
     syslog(LOG_ERR, "ioctl(SIOCSIFFLAGS): %s\n", strerror(errno));
     return 1;
-  }
-  
+  }  
   return 0;
 }
 
@@ -127,9 +130,18 @@ int open_infd(const char device[])
     printf( "setsockopt(in_fd, SO_RCVTIMEO): %s\n", strerror(errno));
     return -1;
   }
-  //close (in_fd) ;
   return in_fd ;
 }
+
+
+
+
+
+
+
+
+
+
 
 int checkup(char * device){
   if (down_radio_interface(device))
@@ -143,4 +155,32 @@ int checkup(char * device){
   in_fd = open_infd(device);
   
   return in_fd;
+}
+
+
+int k_pkt_stats(int in_fd)
+{
+  struct tpacket_stats kstats;
+  socklen_t sl = sizeof (struct tpacket_stats);
+  if (0 != getsockopt(in_fd, SOL_PACKET, PACKET_STATISTICS, &kstats, &sl)) {
+    syslog(LOG_ERR, "getsockopt(PACKET_STATISTICS): %s\n", strerror(errno));
+    return 0;
+  }
+  if (0 == kstats.tp_drops)
+    return 1;
+  if(kstats.tp_drops >0) {
+	printf("no. of drops =%d \n", kstats.tp_drops );
+        exit(0);	
+   }
+  struct timeval now; 
+  struct timeval _tstamp;
+  gettimeofday(&now, NULL);
+  int delay = -1000;
+  if (0 == ioctl(in_fd, SIOCGSTAMP, &_tstamp)) {
+    delay = timeval_to_int64(&now) - timeval_to_int64(&_tstamp);
+  } else {
+    printf("ioctl(SIOCGTSTAMP): %s\n", strerror(errno));
+  }
+  printf( "last %d/%d blocks dropped, block delay is %d ms,", kstats.tp_drops, kstats.tp_packets, delay);
+  return 1;
 }
